486d1f97c4d161363f9af664027982ad815ec2f7
kheris <lounes.kheris@ens.uvsq.fr>
2020-12-15 16:39:31 +0100

    load file + table of car to manipule

--- step2.c
+++ step2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#define MAX_BUFFER 1024
 int find(char*codon, char*seq){
 
 	unsigned char found = 0;
@@ -17,50 +17,63 @@ int find(char*codon, char*seq){
 	return -1;		
 }
 
-int comptecar(){
- int compteur = 0;
+void comptecar(){
  
+ int compteur = 0;
+
  FILE * file;
  
- /* ouverture du fichier */
- if ((file = fopen("6XEZ_P", "r")) == NULL)
-  perror("Erreur à l'ouverture du fichier");
+ 
+ file = fopen("6XEZ_P", "r");
+ if (file == NULL){
+  perror("Erreur à l'ouverture du fichier");}
  
  else {
-   /* parcours du fichier */
-   while(fgetc(file) != EOF)
-    compteur ++; /* incrémentation du compteur */
- }
- 
- printf("Le nombre de caractères du fichier est de : %d\n", compteur);
- 
- /* fermeture du fichier */
- fclose(file);
-
- 
+   int n;
+   char buffer[MAX_BUFFER];
+   while((n = fread(buffer, sizeof(char), MAX_BUFFER, file)))
+      {
+       for (int i = 0; i < n; i++)
+		putchar(buffer[i]);
+   }
+   int tab_car = malloc(sizeof(char) * n);
+   tab_car = buffer;		 
+   return tab_car;
 }
 
 
+ fclose(file); 
+}
 
+
+/*
+int comptecar(){
+ 
+ int compteur = 0;
+ char * tab_car;
+ FILE * file;
+ int nb_car;
+ 
+ file = fopen("6XEZ_P", "r");
+ if (file == NULL){
+  perror("Erreur à l'ouverture du fichier");}
+ 
+ else {
+
+   while(fgetc(file) != EOF)
+    compteur ++; 
+ }
+
+return compteur;
+ 
+
+ fclose(file); 
+}
+*/
 
 int main(){
 
- 	
+	comptecar();
 
 
-
-
-
-
-
-
-
-
-
-   /* char genome[] = "ACTGTUGAUGACTGCTUGCATUCTCAU";
-
-    printf("%d\n", find("AUG", genome));
-	*/
-    return 0;
-    
  }
