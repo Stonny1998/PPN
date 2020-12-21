9e36a4237055f434bec7a83924e54afc8f0eba5f
kheris <lounes.kheris@ens.uvsq.fr>
2020-12-21 02:34:16 +0100

    convert file car to table

--- step2.c
+++ step2.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define MAX_BUFFER 1024
+#include <sys/stat.h>
+
 int find(char*codon, char*seq){
 
 	unsigned char found = 0;
@@ -17,63 +18,39 @@ int find(char*codon, char*seq){
 	return -1;		
 }
 
-void comptecar(){
- 
- int compteur = 0;
+struct stat st;
 
- FILE * file;
- 
- 
- file = fopen("6XEZ_P", "r");
- if (file == NULL){
-  perror("Erreur à l'ouverture du fichier");}
- 
- else {
-   int n;
-   char buffer[MAX_BUFFER];
-   while((n = fread(buffer, sizeof(char), MAX_BUFFER, file)))
-      {
-       for (int i = 0; i < n; i++)
-		putchar(buffer[i]);
-   }
-   int tab_car = malloc(sizeof(char) * n);
-   tab_car = buffer;		 
-   return tab_car;
+char get_carGenome(char *fp){
+FILE * file;
+file = fopen(fp, "r");
+
+ stat(fp, &st);
+ int size = st.st_size;	
+
+
+
+ char *carFile = malloc(sizeof(char) * size);
+   int i=0;
+   while ( ! feof( file ) && (i<size) ) {
+        int theCurrentChar;        
+        theCurrentChar = fgetc( file );
+         carFile[i] = theCurrentChar;
+
+         i++;
+    }	
+  printf("%d", find("ATG", carFile));
+     
+   fclose(file); 
+
 }
 
-
- fclose(file); 
-}
-
-
-/*
-int comptecar(){
- 
- int compteur = 0;
- char * tab_car;
- FILE * file;
- int nb_car;
- 
- file = fopen("6XEZ_P", "r");
- if (file == NULL){
-  perror("Erreur à l'ouverture du fichier");}
- 
- else {
-
-   while(fgetc(file) != EOF)
-    compteur ++; 
- }
-
-return compteur;
- 
-
- fclose(file); 
-}
-*/
 
 int main(){
+ 
+ get_carGenome("MT873231.1");
 
-	comptecar();
+   }	
+	
 
-
- }
+ 
+ 
